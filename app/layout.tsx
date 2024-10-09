8d9074d344a2a01bd7acb40deaf7e32647d70eec
Guilherme Sales <gnailpo895@gmail.com>
2024-10-09 17:25:19 +0100

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import navbar from "@/components/navbar";
+import footer from "@/components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <navbar
+      <navbar/>
+
+      <footer/>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-bold text-teal-500">
+    <h1 className="text-bold text-teal-500 text-2x1">
       Hello World
     </h1>
   );
