afb0d38dee8402d7df92d4c53b1302ad895b8016
Guilherme Sales <gnailpo895@gmail.com>
2024-10-09 18:17:33 +0100

    Adicao de uma pasta com imagens para o site

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 //para imprimir isto basta fazer "rafce" que isto ira fazer automatico
 const Navbar = () => {
@@ -5,7 +6,7 @@ const Navbar = () => {
     <nav className="border-2 border-red-500 flexBetween max-container 
     padding-container relative z-30 py-5">
       <link href="/">
-        
+        <Image src = "/hilink-logo.svg" alt="logo" width={74} height={29}/>
       </link>
     </nav>
   )
